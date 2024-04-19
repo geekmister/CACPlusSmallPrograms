955656525c6c177723e7346f2b6c4f895a736a67
Geekmister <geekmister.mg@gmail.com>
2024-04-19 18:01:34 +0800

    feat(practice): Add two practice that HelloWorld and PrintfFiled.

---
+++ practice/PrintfFiled/PrintfFiled.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+    // The result of printf() is not like 田, but like a table. Forgave me.
+    printf("|--|--|\n");
+    printf("|--|--|\n");
+    printf("|--|--|");
+    return 0;
+}
